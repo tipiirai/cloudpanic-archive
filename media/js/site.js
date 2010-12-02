7ff8d2770b4f6bdf863aa8fedcf7381cd2fd1d7f
Tero Piirainen <tipiirai@gmail.com>
2010-12-02 07:38:49 +0000

    update

--- media/js/site.js
+++ media/js/site.js
@@ -18,9 +18,9 @@
 		return diff < 86400 && (
 				 diff < 60 && "just now" ||
 				 diff < 120 && (future ? "a minute" : "1 minute ago") ||
-				 diff < 3600 && str("_ minutes", "_ minutes ago", Math.floor( diff / 60 )) ||
-				 diff < 7200 && (future ? "one hour" : "1 hour ago") ||
-				 diff < 86400 && str("_ hours", "_ hours ago", Math.floor(diff / 3600))) ||
+				 diff < 3600 && str("_ min", "_ minutes ago", Math.floor( diff / 60 )) ||
+				 diff < 7200 && (future ? "<span>1</span> hour" : "1 hour ago") ||
+				 diff < 86400 && str("_hours", "_ hours ago", Math.floor(diff / 3600))) ||
 				 day_diff == 1 && (future ? "Tomorrow" : "Yesterday") ||
 				 day_diff < 7 && str("_ days", "_ days ago", day_diff) ||
 				 day_diff == 7 && (future ? "a week" : "week ago") ||
